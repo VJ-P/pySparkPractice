4173433e59cf91cdc70ccd1c95607c5f7fe51e98
Vijay Patel <108.vijayp@gmail.com>
2024-04-20 15:34:24 -0400

    Create Spark job to find max temperatures

---
+++ max-temperatures.py
@@ -0,0 +1,36 @@
+'''
+Input: WeatherStationID, DateYYYYMMDD, ObservationType, Value (Tenth of a Degree Celcius -75 -> -7.5C),,,
+ITE00100554,18000101,TMAX,-75,,,E,
+ITE00100554,18000101,TMIN,-148,,,E,
+GM000010962,18000101,PRCP,0,,,E,
+EZE00100082,18000101,TMAX,-86,,,E,
+EZE00100082,18000101,TMIN,-135,,,E,
+
+Output: Maximum Temperature in a Year For Each Weather Station
+'''
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("MaxTemp")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    stationID = fields[0]
+    entryType = fields[2]
+    temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
+    return (stationID, entryType, temperature)
+
+lines = sc.textFile("C:/Users/vijay/Documents/SparkCourse/1800.csv")
+parsedLines = lines.map(parseLine)
+
+# Removes any non TMAX records
+maxTemps = parsedLines.filter(lambda x: "TMAX" in x[1])
+# We don't need the ObservationType column anymore since its all TMAX, so remove it
+stationTemps = maxTemps.map(lambda x: (x[0], x[2]))
+# Get the highest temp for each station
+maxTemps = stationTemps.reduceByKey(lambda x, y: max(x,y))
+
+results = maxTemps.collect()
+
+for res in results:
+    print(res[0] + "\t{:.2f}F".format(res[1]))
